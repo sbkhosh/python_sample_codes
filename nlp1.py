b85591f123e38be062d49b7f0a545c7ed0172fb5
sk <49952299+sbkhosh@users.noreply.github.com>
2019-10-08 18:25:11 +0200

    Deleting for update

--- nlp1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import urllib2
-# urllib2 is use to download the html content of the web link
-response = urllib2.urlopen('http://python.org/')
-# You can read the entire content of a file using read() method
-html = response.read()
-print(html)
